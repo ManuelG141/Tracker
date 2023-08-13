18735a162bcdfce3d72e22cd4e9e0f253335177a
Manuel <manuelgerman1105@gmail.com>
2023-08-13 17:46:05 -0500

    ADDING THE FUNCTIONALITY TO SEND DATA OVER UCP PROTOCOL

--- app/src/main/java/edu/uninorte/tracker/MainActivity.kt
+++ app/src/main/java/edu/uninorte/tracker/MainActivity.kt
@@ -2,8 +2,10 @@ package edu.uninorte.tracker
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.location.LocationManager.*
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -13,17 +15,47 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+class UdpSender {
+    fun sendUdpMessage(message: String, ipAddress: String, port: Int) {
+        try {
+            val udpSocket = DatagramSocket()
+            val dataToSend = message.toByteArray()
+            val address = InetAddress.getByName(ipAddress)
+            val sendPacket = DatagramPacket(dataToSend, dataToSend.size, address, port)
+
+            Log.d("UDP", "sending data over UDP")
+            udpSocket.send(sendPacket)
+            Log.d("UDP", "data send over UDP")
+            udpSocket.close()
+            Log.d("UDP", "socket closed")
+        } catch (e: Exception) {
+            Log.d("UDP", "Exception")
+            e.printStackTrace()
+        }
+    }
+}
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationManager: LocationManager
+
     private lateinit var latitudeValue: TextView
     private lateinit var longitudeValue: TextView
     private lateinit var timeStampValue: TextView
     private lateinit var providerValue: TextView
     private lateinit var ipAddressValue: EditText
+    private lateinit var portNumberValue: EditText
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,35 +63,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         latitudeValue = findViewById(R.id.latitude_value)
         longitudeValue = findViewById(R.id.longitude_value)
         timeStampValue = findViewById(R.id.timeStamp_value)
         providerValue = findViewById(R.id.provider_value)
         ipAddressValue = findViewById(R.id.ip_address_editText)
+        portNumberValue = findViewById(R.id.port_number_editText)
 
         val getLocationButton = findViewById<Button>(R.id.Get_Location_Button)
         val sendTCPButton = findViewById<Button>(R.id.tcp_send_data_button)
         val sendUDPButton = findViewById<Button>(R.id.udp_send_data_button)
 
+        val udpSender = UdpSender()
+        var ipAddress: String
+        var portNumber: Int
+        var message: String
+
+
         getLocationButton.setOnClickListener {
             checkLocationPermissions()
         }
         sendTCPButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                "sending data using TCP (coming soon)",
-                Toast.LENGTH_SHORT
-            )
-                .show()
+            showPopUp("sending data over TCP protocol (coming soon)")
         }
         sendUDPButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                "sending data using UDP (coming soon)",
-                Toast.LENGTH_SHORT
-            )
-                .show()
+            showPopUp("sending data over UDP protocol")
+            ipAddress = ipAddressValue.text.toString()
+            portNumber = stringToInt(portNumberValue.text.toString())
+            Log.d("UDP", "")
+            message = "${latitudeValue.text},${longitudeValue.text},${timeStampValue.text}"
+            showPopUp("ipAddress:$ipAddress,message:$message")
+            lifecycleScope.launch(Dispatchers.IO){
+                udpSender.sendUdpMessage(message, ipAddress, portNumber)
+            }
+            showPopUp("data send over UDP protocol")
         }
+    }
+
+
+    private fun stringToInt(string: String): Int{
+        val intValue = try {
+            string.toInt()
+        } catch (e: NumberFormatException) {
+            0
+        }
+        return intValue
+    }
+    private fun showPopUp(message: String){
+        Toast.makeText(
+            this@MainActivity,
+            message,
+            Toast.LENGTH_SHORT
+        )
+            .show()
     }
 
     //This function contains the logic for requesting Location permissions
@@ -75,7 +133,7 @@ class MainActivity : AppCompatActivity() {
             )
         ) {
             //location permissions have been rejected
-            Toast.makeText(this, "Location permissions rejected", Toast.LENGTH_SHORT).show()
+            showPopUp("Location permissions rejected")
         } else {
             //Request location permissions
             ActivityCompat.requestPermissions(
@@ -109,26 +167,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     @SuppressLint("MissingPermission")
     private fun getUserLocation() {
-        Toast.makeText(this, "Getting location", Toast.LENGTH_SHORT).show()
-        val location = fusedLocationProviderClient.lastLocation
+        showPopUp("Getting location")
+        val location = this.fusedLocationProviderClient.lastLocation
         location.addOnSuccessListener {
             if (it != null) {
                 latitudeValue.text = it.latitude.toString()
                 longitudeValue.text = it.longitude.toString()
                 timeStampValue.text = it.time.toString()
-                providerValue.text = LocationManager.GPS_PROVIDER
-                Log.d(
-                    "location",
-                    "lat: ${it.latitude}" +
-                            "lon: ${it.longitude}" +
-                            "time: ${it.time} " +
-                            "PackageManager.FEATURE_LOCATION_GPS: ${LocationManager.GPS_PROVIDER}"
-                )
+                providerValue.text = it.provider
             }
-            Toast.makeText(this, "Location Got", Toast.LENGTH_SHORT).show()
+            showPopUp("Location Got")
         }
+        for (element in locationManager.allProviders) Log.d("Location", element)
     }
 
     @Deprecated("Deprecated in Java")
@@ -148,12 +201,7 @@ class MainActivity : AppCompatActivity() {
                 getUserLocation()
             } else {
                 //The permissions have not been accepted
-                Toast.makeText(
-                    this,
-                    "Location Permissions rejected for the first time",
-                    Toast.LENGTH_SHORT
-                )
-                    .show()
+                showPopUp("Location Permissions rejected for the first time")
             }
         }
     }
